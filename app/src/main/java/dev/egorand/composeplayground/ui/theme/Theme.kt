335755b57533358887a1d7ff191bb5f06bc7c6d9
Egor Andreevici <egor@squareup.com>
2022-06-04 12:36:48 -0400

    Random code cleanup

--- app/src/main/java/dev/egorand/composeplayground/ui/theme/Theme.kt
+++ app/src/main/java/dev/egorand/composeplayground/ui/theme/Theme.kt
@@ -10,10 +10,11 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
   primary = Purple80,
@@ -24,17 +25,14 @@ private val DarkColorScheme = darkColorScheme(
 private val LightColorScheme = lightColorScheme(
   primary = Purple40,
   secondary = PurpleGrey40,
-  tertiary = Pink40
-
-  /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+  tertiary = Pink40,
+  background = Color(0xFFFFFBFE),
+  surface = Color(0xFFFFFBFE),
+  onPrimary = Color.White,
+  onSecondary = Color.White,
+  onTertiary = Color.White,
+  onBackground = Color(0xFF1C1B1F),
+  onSurface = Color(0xFF1C1B1F),
 )
 
 @Composable
@@ -55,8 +53,9 @@ fun ComposePlaygroundTheme(
   val view = LocalView.current
   if (!view.isInEditMode) {
     SideEffect {
-      (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-      ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+      val window = (view.context as Activity).window
+      window.statusBarColor = colorScheme.primary.toArgb()
+      WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
     }
   }
 
